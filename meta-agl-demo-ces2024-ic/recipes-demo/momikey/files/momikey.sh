8095aa91b8a754de9404ff32137c4a86488b3c0f
Hiroyuki Ishii <ishii.hiroyuki002@jp.panasonic.com>
2023-12-27 09:14:07 +0900

    momikey: Grub any kind of usb keyboards

--- meta-agl-demo-ces2024-ic/recipes-demo/momikey/files/momikey.sh
+++ meta-agl-demo-ces2024-ic/recipes-demo/momikey/files/momikey.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-while sleep 1 ;do evtest /dev/input/by-id/usb-wch.cn_CH57x-event-kbd ;done | while read line; do
+known_key_devices=(
+    /dev/input/by-id/usb-wch.cn_CH57x-event-kbd
+    /dev/input/by-id/usb-1189_8890-if02-event-kbd
+)
+
+event_reader () {
+    local device target_device
+    # try to grub any kind of usb keyboard
+    known_key_devices+=(/dev/input/by-id/usb-*-kbd)
+    for device in "${known_key_devices[@]}"; do
+        [ -e "$device" ] && target_device="$device" && break
+    done
+    [ -z "$target_device" ] && return
+    evtest "$target_device"
+}
+
+while sleep 1 ;do event_reader ;done | while read line; do
     unset key target_container
     [[ "$line" =~ \((KEY_.)\),\ value\ 1 ]] && key="${BASH_REMATCH[1]}"
     case "$key" in
